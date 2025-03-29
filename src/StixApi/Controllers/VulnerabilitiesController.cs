0883c78f1ced193513b150d8b51e5fc11853ad19
Stefan Alfbo <stefan@alfbo.se>
2025-03-29 12:54:26 +0000

    Add stub endpoints for the needed endpoints in the project

--- src/StixApi/Controllers/VulnerabilitiesController.cs
+++ src/StixApi/Controllers/VulnerabilitiesController.cs
@@ -3,14 +3,9 @@ using Microsoft.AspNetCore.Mvc;
 namespace StixApi.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("v1/api/[controller]")]
 public class VulnerabilitiesController : ControllerBase
 {
-    private static readonly string[] Summaries = new[]
-    {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
-
     private readonly ILogger<VulnerabilitiesController> _logger;
 
     public VulnerabilitiesController(ILogger<VulnerabilitiesController> logger)
@@ -18,13 +13,52 @@ public class VulnerabilitiesController : ControllerBase
         _logger = logger;
     }
 
-    [HttpGet(Name = "GetVulnerability")]
-    public IEnumerable<Vulnerability> Get()
+    // GET: v1/api/vulnerabilities - This endpoint retrieves a list of all vulnerabilities in the
+    // system.
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Vulnerability>>> Get()
     {
-        return Enumerable.Range(1, 5).Select(index => new Vulnerability
+        _logger.LogInformation("Retrieving all vulnerabilities.");
+        return Ok(Enumerable.Range(1, 5).Select(index => new Vulnerability
         {
             Name = "Name"
-        })
-        .ToArray();
+        }));
+    }
+
+    // GET: v1/api/vulnerabilities/{id} - This endpoint should retrieve a specific vulnerability by its ID.
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Vulnerability>> Get(int id)
+    {
+        _logger.LogInformation($"Retrieving vulnerability with ID: {id}.");
+        return Ok(new Vulnerability
+        {
+            Name = "Name"
+        });
+    }
+
+    // POST: v1/api/vulnerabilities - This endpoint should create a new vulnerability using the STIX II
+    // vulnerability model.
+    public async Task<ActionResult<int>> Post([FromBody] Vulnerability vulnerability)
+    {
+        _logger.LogInformation("Creating a new vulnerability.");
+        return Ok(1);
+    }
+
+    // PUT: v1/api/vulnerabilities/{id} - This endpoint should update an existing vulnerability by its ID
+    // using the STIX II vulnerability model.
+    [HttpPut("{id}")]
+    public async Task<ActionResult> Put(int id, [FromBody] Vulnerability vulnerability)
+    {
+        _logger.LogInformation($"Updating vulnerability with ID: {id}.");
+        return NoContent();
+    }
+
+    // DELETE: v1/api/vulnerabilities/{id} - This endpoint should delete an existing vulnerability by its
+    // ID.
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> Delete(int id)
+    {
+        _logger.LogInformation($"Deleting vulnerability with ID: {id}.");
+        return NoContent();
     }
 }
